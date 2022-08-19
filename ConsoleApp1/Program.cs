9d389e02da0aa837e9a241ef086bcd8b217bd3eb
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-20 00:28:20 +0530

    [jayesh]  fibannoci series

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleApp1;
+using System;
+
+namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Fibonacci FiboNacci = new Fibonacci();
+            FiboNacci.FibonacciSeries();
+        }
+    }
+}
