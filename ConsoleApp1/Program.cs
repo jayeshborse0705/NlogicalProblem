1da3be99fbeb3e0f7dfa07a9f4b1b3a30928cfd8
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-20 00:38:04 +0530

    [jayesh]  perfect Number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,9 +7,27 @@ namespace LogicalProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter number as per options");
+            Console.WriteLine("1: For fibonacci series");
+            Console.WriteLine("2: For perfect number");
 
-            Fibonacci FiboNacci = new Fibonacci();
-            FiboNacci.FibonacciSeries();
+            int check = Convert.ToInt32(Console.ReadLine());
+            switch (check)
+            {
+
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.FibonacciSeries();
+                    break;
+                case 2:
+                    PerfectNum perfectNum = new PerfectNum();
+                    perfectNum.Perfect_Num();
+                    break;
+                default:
+                    Console.WriteLine("Please SELECT correct number ! ");
+                    break;
+
+            }
         }
     }
 }
