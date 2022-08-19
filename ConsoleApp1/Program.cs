666e3e6be69a3dd39e07b3e18e0ee2be1696f350
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-08-20 00:45:50 +0530

    [jayesh]  prime Number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApp1;
+using LogicalProblem;
 using System;
 
 namespace LogicalProgram
@@ -10,6 +11,8 @@ namespace LogicalProgram
             Console.WriteLine("Please enter number as per options");
             Console.WriteLine("1: For fibonacci series");
             Console.WriteLine("2: For perfect number");
+            Console.WriteLine("3: For prime number");
+
 
             int check = Convert.ToInt32(Console.ReadLine());
             switch (check)
@@ -22,6 +25,10 @@ namespace LogicalProgram
                 case 2:
                     PerfectNum perfectNum = new PerfectNum();
                     perfectNum.Perfect_Num();
+                    break;
+                case 3:
+                    PrimeNumber primeNum = new PrimeNumber();
+                    primeNum.PrimeNum();
                     break;
                 default:
                     Console.WriteLine("Please SELECT correct number ! ");
